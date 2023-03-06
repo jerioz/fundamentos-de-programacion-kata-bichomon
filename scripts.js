e7c373e891bb62939a92f377afb9125ad42651c7
jerioz <escobedoranero@gmail.com>
2023-03-06 19:54:56 +0100

    bichomon changes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-console.log(document.title);
+const changeName = document.getElementById('gen-1')
+changeName.innerHTML = "Generasión Pokimon"
+
+const changeColor = document.querySelector('.infocard-list')
+changeColor.style.backgroundColor = "pink"
+
+console.log(window.document.location.href)
+
+console.log(window.document.location.hostname)
+
+console.log(document.getElementsByTagName('img'))
+
+const img = document.querySelectorAll('img')
+.forEach(img => img.src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif");
+console.log(img)
+
+
+
+
+
